4c0f41b0a508ea8db371b0e4a805678c9a377f74
2cong <cjina0418@gmail.com>
2023-01-01 21:14:59 +0900

    2째주_coding_test

--- programmers/level2/week1.py
+++ programmers/level2/week1.py
@@ -1,3 +1,4 @@
+# 주차 요금 계산 (https://school.programmers.co.kr/learn/courses/30/lessons/92341)
 import math
 from collections import defaultdict
 
@@ -50,5 +51,31 @@ def solution(fees, records):
             during_time = during_time + calc_time(out_time, in_time)
         fee = get_fee(fees, during_time)
         answer.append(fee)
+
+    return answer
+
+
+# 튜플 (https://school.programmers.co.kr/learn/courses/30/lessons/64065)
+def solution(s):
+    s = s.split('},')
+    tuples = []
+
+    ordered_answer = {}
+
+    for tuple_member in s:
+        tuple_member = tuple_member.replace('{', '').replace('}', '').replace("'", '')
+        tuple = [int(member) for member in tuple_member.split(',')]
+        tuples.append(tuple)
+
+    sorted_tuples = sorted(tuples, key=lambda t: len(t))
+
+    for idx, tuple in enumerate(sorted_tuples):
+        if idx == 0:
+            ordered_answer[tuple[0]] = idx
+
+        sorted_tuple = sorted(tuple, key=lambda t: ordered_answer.get(t, len(tuple)))
+        ordered_answer[sorted_tuple[idx]] = idx
+
+    answer = list(ordered_answer.keys())
 
     return answer
